db00bd2d1d59bb3f8990c59939e98608a2846ee0
Lucas Nakagawa <lucasnakagawanascimento@gmail.com>
2024-04-24 16:54:01 -0300

    feat: Add WPF application.

---
+++ Graphic.Interface/StockInfo.xaml.cs
@@ -0,0 +1,37 @@
+﻿using StockCrawler.Service;
+using System.Windows;
+
+namespace Graphic.Interface
+{
+    /// <summary>
+    /// Interaction logic for StockInfo.xaml
+    /// </summary>
+    public partial class StockInfo : Window
+    {
+        private readonly AssetsService _assetsService;
+
+        public StockInfo()
+        {
+            InitializeComponent();
+            _assetsService = new AssetsService();
+        }
+
+        private async void btnGetAssets_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                btnGetAssets.IsEnabled = false;
+                var assets = await Task.Run(() => _assetsService.GetWalletAssets());
+                dgAssets.ItemsSource = assets;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                btnGetAssets.IsEnabled = true;
+            }
+        }
+    }
+}
